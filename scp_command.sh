77d3ca89cf369c780bb89aac5e976cdf6f80247a
Rui Luo <u8076655@anu.edu.au>
2025-04-19 14:19:56 +1000

    command

--- scp_command.sh
+++ scp_command.sh
@@ -1,5 +1,10 @@
 MY_IP=192.168.1.88
-scp -i ~/.ssh/id_rsa -r paperspace@$MY_IP:/home/paperspace/CV_MINI_PROJECT/train_metrics/ .
-scp -i ~/.ssh/id_rsa -r paperspace@$MY_IP:/home/paperspace/CV_MINI_PROJECT/metrics_finetune/ .
-scp -i ~/.ssh/id_rsa -r paperspace@$MY_IP:/home/paperspace/CV_MINI_PROJECT/final_model_result/ .
-scp -i ~/.ssh/id_rsa -r paperspace@$MY_IP:/home/paperspace/CV_MINI_PROJECT/finetune_model_result/ .
+REMOTE_USER=paperspace
+OUTPUT_DIR=./cv/base  # 本地输出目录
+
+mkdir -p $OUTPUT_DIR  # 如果目录不存在则创建
+
+scp -i ~/.ssh/id_rsa -r $REMOTE_USER@$MY_IP:/home/paperspace/CV_MINI_PROJECT/train_metrics/         $OUTPUT_DIR/
+scp -i ~/.ssh/id_rsa -r $REMOTE_USER@$MY_IP:/home/paperspace/CV_MINI_PROJECT/metrics_finetune/      $OUTPUT_DIR/
+scp -i ~/.ssh/id_rsa -r $REMOTE_USER@$MY_IP:/home/paperspace/CV_MINI_PROJECT/final_model_result/    $OUTPUT_DIR/
+scp -i ~/.ssh/id_rsa -r $REMOTE_USER@$MY_IP:/home/paperspace/CV_MINI_PROJECT/finetune_model_result/ $OUTPUT_DIR/
